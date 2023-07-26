72f8b3dbc048b1b508068162ce426b956b882b04
Luciano Ruiz Celidonio <luciano.celidonio@gmail.com>
2023-07-26 17:21:17 -0300

    i1

--- receitas/views.py
+++ receitas/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'receitas/home.html', context={'name': 'Luciano', })
+
+    # return render(request, 'receitas/pages/home.html')
+    return render(request, 'receitas/pages/home.html', context={
+        'name': 'Luciano',
+    })
